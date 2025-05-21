f2309d8954f172169b558ccc80264ab8ecf51c13
elton <eltonaureliano@hotmail.com>
2025-05-20 21:33:39 -0300

    Alterado frase

--- src/main/java/com/Exercicio07.java
+++ src/main/java/com/Exercicio07.java
@@ -6,32 +6,25 @@ public class Exercicio07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite o primeiro número inteiro: ");
+        System.out.print("Digite o primeiro número inteiro:");
         int numero1 = scanner.nextInt();
 
-        System.out.print("Digite o segundo número inteiro: ");
+        System.out.print("Digite o segundo número inteiro:");
         int numero2 = scanner.nextInt();
 
         int soma = numero1 + numero2;
         int subtracao = numero1 - numero2;
         int multiplicacao = numero1 * numero2;
-        double divisao;
+        double divisao = (double) numero1 / numero2;
 
-        if (numero2 != 0) {
-            divisao = (double) numero1 / numero2;
-        } else {
-            System.out.println("Divisão por zero não é permitida.");
-            scanner.close();
-            return;
+            System.out.println("Divisão por zero não é permitida");
+        System.out. println ("Soma: " + soma + " Subtração: " + subtracao + " + Multiplicao: + "multiplicação + Divisão");
+
         }
-        
-        System.out.println("Soma: " + soma + ", Subtração: " + subtracao + ", Multiplicação: " + multiplicacao + ", Divisão: " + divisao);
-
-        scanner.close();
     }
-}
 
 
 
-    }
-}
+
+
+
